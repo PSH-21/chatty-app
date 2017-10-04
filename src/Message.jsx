97acdc4bf65a4fa7c5169db6ef273501f32b9fa5
Peter Hunt <peter.s.hunt@gmail.com>
2017-10-04 19:47:06 +0000

    spacing fixed

--- src/Message.jsx
+++ src/Message.jsx
@@ -11,7 +11,7 @@ class Message extends Component {
           <div class="message">
             <span className="message-username">Anonymous1</span>
             <span className="message-content">I won't be impressed with technology until I can download food.</span>
-         </div>
+          </div>
         </main>
 
     )
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -1,14 +1,14 @@
 import React, {Component} from 'react';
 import Message from './Message.jsx';
 
- class MessageList extends Component {
-   render() {
-     return (
+class MessageList extends Component {
+  render() {
+    return (
       <div classname="message-list">
         <Message/>
       </div>
-     )
-   }
- }
+    )
+  }
+}
 
- export default MessageList;
+export default MessageList;
