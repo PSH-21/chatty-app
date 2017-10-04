166804fbb9a11bc8aa71763cd7700892574180f7
Peter Hunt <peter.s.hunt@gmail.com>
2017-10-04 20:42:25 +0000

    add state and props; iterate over messages passing items through props; add key ids to messages

--- src/Message.jsx
+++ src/Message.jsx
@@ -5,12 +5,15 @@ import React, {Component} from 'react';
 </div>
 
 class Message extends Component {
+  constructor(props) {
+    super(props);
+  }
   render() {
     return (
        <main className="messages">
-          <div class="message">
-            <span className="message-username">Anonymous1</span>
-            <span className="message-content">I won't be impressed with technology until I can download food.</span>
+          <div className="message">
+            <span className="message-username">{this.props.username}</span>
+            <span className="message-content">{this.props.content}</span>
           </div>
         </main>
 
--- src/MessageList.jsx
+++ src/MessageList.jsx
@@ -2,13 +2,38 @@ import React, {Component} from 'react';
 import Message from './Message.jsx';
 
 class MessageList extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+
+      currentUser: {name: "Bob"}, // optional. if currentUser is not defined, it means the user is Anonymous
+      messages: [
+        {
+          id: 579318,
+          username: "Bob",
+          content: "Has anyone seen my marbles?",
+        },
+        {
+          id: 579319,
+          username: "Anonymous",
+          content: "No, I think you lost them. You lost your marbles Bob. You lost them for good."
+        }
+      ]
+    }
+
+  }
+
   render() {
     return (
-      <div classname="message-list">
-        <Message/>
+      <div className="message-list">
+        {this.state.messages.map(item => {
+          return <Message username={item.username} content={item.content} key={item.id} />
+
+        })}
       </div>
     )
   }
+
 }
 
 export default MessageList;
